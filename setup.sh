978e25cf80a9f256f40a0c93ddf574befe42313c
Mateusz Klos <novopl@gmail.com>
2022-09-12 11:27:29 +0100

    Fix installation script

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ REPO_DIR=$(dirname $(readlink -fm "$0"))
 
 echo $REPO_DIR
 
-[ ! -e "${REPO_DIR}/.vim" ]       && ln -s "${REPO_DIR}/vim"         "${HOME}/.vim"
-[ ! -e "${REPO_DIR}/.vimrc" ]     && ln -s "${REPO_DIR}/vimrc"       "${HOME}/.vimrc"
+[ ! -e "${HOME}/.vim" ]       && ln -s "${REPO_DIR}/vim"         "${HOME}/.vim"       || echo ".vim already exists, skipping..."
+[ ! -e "${HOME}/.vimrc" ]     && ln -s "${REPO_DIR}/vimrc"       "${HOME}/.vimrc"     || echo ".vimrc already exists, skipping..."
 
